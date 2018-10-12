f5e33af4db0f10ff33bc0a7bd5f9057f26dd6d4d
nthyagarajan <Thyagarajan.Natarajan@silabs.com>
2018-10-12 15:06:35 +0800

    Add files via upload
    Hosting the PyZware documentation here.

---
+++ search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['managing_20smart_20start_20devices_20in_20the_20network',['Managing smart start devices in the network',['../group__smartstart.html',1,'']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cdimpwz",
+  1: "z",
+  2: "cdimpw",
+  3: "z"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "groups",
+  3: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Modules",
+  3: "Pages"
+};
+
